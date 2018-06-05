248f3e7fe294bfec7efadb09499b6fd0cec7b04a
maaslalaniii <maas@live.ca>
2018-06-05 17:22:19 -0400

    🎉 Initial commit

---
+++ cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// imports
+let generate = require('./commands/generate') 
+
+// parse command line arguments
+let command  = process.argv[2]
+
+switch (command) {
+
+  case 'generate':
+  case 'g':
+    generate()
+    break
+
+  default:
+    console.log('Invalid command syntax. Type \'help\' to see correct syntax.')
+
+}
---
+++ commands/generate.js
@@ -0,0 +1,25 @@
+// import filesystem to read text files
+const filesystem = require('fs')
+
+// read ideas list and store in an array
+let ideas = filesystem
+				.readFileSync(`${__dirname}/../data/ideas.text`, 'utf-8')
+				.split('\r\n')
+
+// read audiences list and store in an array
+let audiences = filesystem
+				.readFileSync(`${__dirname}/../data/audiences.text`, 'utf-8')
+				.split('\r\n')
+
+// generate a startup
+function generate() {
+	console.log(`${ideas.random()} for ${audiences.random()}`)
+}
+
+// helper function for obtaining random elements from lists
+Array.prototype.random = function () {
+	return this[Math.floor((Math.random()*this.length))];
+}
+
+// export command to be used in cli
+module.exports = generate
