2f477614d9ab02002daf0f3fd05f5926f79cbfcc
maaslalaniii <maas@live.ca>
2018-06-06 18:40:31 -0400

    Fix indentation, convert tabs to spaces

--- commands/generate.js
+++ commands/generate.js
@@ -3,22 +3,22 @@ const filesystem = require('fs')
 
 // read ideas list and store in an array
 let ideas = filesystem
-				.readFileSync(`${__dirname}/../data/ideas.text`, 'utf-8')
-				.split('\r\n')
+              .readFileSync(`${__dirname}/../data/ideas.text`, 'utf-8')
+              .split('\r\n')
 
 // read audiences list and store in an array
 let audiences = filesystem
-				.readFileSync(`${__dirname}/../data/audiences.text`, 'utf-8')
-				.split('\r\n')
+                  .readFileSync(`${__dirname}/../data/audiences.text`, 'utf-8')
+                  .split('\r\n')
 
 // generate a startup
 function generate() {
-	console.log(`${ideas.random()} for ${audiences.random()}`)
+  console.log(`${ideas.random()} for ${audiences.random()}`)
 }
 
 // helper function for obtaining random elements from lists
 Array.prototype.random = function () {
-	return this[Math.floor((Math.random()*this.length))];
+  return this[Math.floor((Math.random()*this.length))];
 }
 
 // export command to be used in cli
